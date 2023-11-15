33c1a7248ff33fa60d183c23e2c245ef07c2cd87
Monika Januszek <monika.januszek.mail@gmall.com>
2023-11-15 14:18:53 +0100

    New Entry komponent

--- src/NewEntry.jsx
+++ src/NewEntry.jsx
@@ -1,37 +1,58 @@
-import ReactFlow, { useNodesState, useEdgesState, addEdge } from "reactflow";
-import "reactflow/dist/style.css";
+import { useState, useEffect } from "react";
 
-const initialNodes = [
-  { id: "1", position: { x: 0, y: 0 }, data: { label: "12212121212" } },
-  { id: "2", position: { x: 0, y: 70 }, data: { label: "212121212121212" } },
-];
-const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
+const temporaryPlaceForEntries = [];
 
-function NewEntry() {
-  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
-  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
-
-  //   const onConnect = useCallback(
-  //     (params) => setEdges((eds) => addEdge(params, eds)),
-  //     [setEdges]
-  //   );
+function NewEnry() {
+  // const [photo, setPhoto] = useState();
+  // const [placeName, setPlaceName] = useState();
+  const [newEntry, setNewEntry] = useState({
+    name: "",
+    date: "",
+    photo: null,
+  });
+  function handleAddNewEntry(e) {
+    e.preventDefault();
+    console.log(newEntry);
+  }
   return (
     <>
       <h3>Add new entry to your journal</h3>
-      <div>Date: </div>
-      <div style={{ width: "80vw", height: "80vh", border: "2px solid black" }}>
-        Stop name:
-        <ReactFlow
-          nodes={nodes}
-          edges={edges}
-          //   onNodesChange={onNodesChange}
-          //   onEdgesChange={onEdgesChange}
-          //   onConnect={onConnect}
+      <form action="">
+        <label htmlFor="trip-date"></label>
+        <input
+          type="date"
+          id="trip-date"
+          onChange={(e) => {
+            setNewEntry((prevEntry) => ({
+              ...prevEntry,
+              date: e.target.value,
+            }));
+          }}
         />
-      </div>
-      <div>Upload photo:</div>
+
+        <input
+          type="text"
+          placeholder="Name of place you visited"
+          onChange={(e) => {
+            setNewEntry((prevEntry) => ({
+              ...prevEntry,
+              name: e.target.value,
+            }));
+          }}
+        />
+        <hr />
+        <label htmlFor="trip-photo">Add photo</label>
+        <input
+          type="file"
+          name=""
+          id="trip-photo"
+          style={{ border: "1px solid black" }}
+        />
+        <hr />
+        <button onClick={handleAddNewEntry}>Add new memory</button>
+      </form>
     </>
   );
 }
 
-export default NewEntry;
+export default NewEnry;
